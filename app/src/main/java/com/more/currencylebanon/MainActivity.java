a5794acefb4d4d01acae0a73c98455e7e73d8694
ahmed <devahmedmoner@gmail.com>
2021-11-16 22:03:36 +0200

    ahmed  moner

--- app/src/main/java/com/more/currencylebanon/MainActivity.java
+++ app/src/main/java/com/more/currencylebanon/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "ahmed moner", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "ahmed moner", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "ahmed moner", Toast.LENGTH_SHORT).show();
     }
 }
