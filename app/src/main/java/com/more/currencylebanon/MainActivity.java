163cacbcba473eb9216c8f186af874c81ee27666
ahmed <devahmedmoner@gmail.com>
2021-11-16 22:20:55 +0200

    ahmed  moner fdd

--- app/src/main/java/com/more/currencylebanon/MainActivity.java
+++ app/src/main/java/com/more/currencylebanon/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // Cannot run program "svn" (in directory "F:\Currency-Lebanon"): CreateProcess error=2, The system cannot find the file specified
       //ahmed moner
     }
 }
