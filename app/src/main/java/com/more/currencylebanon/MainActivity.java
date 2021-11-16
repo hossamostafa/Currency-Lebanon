39a234c666f7313ebc7b36c43cfa638a886e38cd
ahmed <devahmedmoner@gmail.com>
2021-11-16 22:00:30 +0200

    ahmed

--- app/src/main/java/com/more/currencylebanon/MainActivity.java
+++ app/src/main/java/com/more/currencylebanon/MainActivity.java
@@ -2,6 +2,7 @@ package com.more.currencylebanon;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "ahmed moner", Toast.LENGTH_SHORT).show();
     }
 }
